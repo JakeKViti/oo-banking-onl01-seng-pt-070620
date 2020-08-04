562244958752ae077794e045957e0f3705258024
Jacob Viti <thevitij64@gmail.com>
2020-08-04 17:10:43 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,38 @@
 class Transfer
-  # your code here
+  attr_accessor :sender, :status, :receiver, :amount
+
+  def initialize(sender, receiver, amount)
+    @sender = sender
+    @receiver = receiver
+    @status = "pending"
+    @amount = amount
+  end
+
+  def valid?
+    if @sender.valid? && @receiver.valid?
+      return true
+    else
+      return false
+    end
+  end
+
+  def execute_transaction
+    if valid? == false
+      return "Transaction rejected. Please check your account balance."
+      @status = "rejected"
+    end
+    counter = 0
+    while counter >= 0
+    @sender.deposit(-@amount)
+    @receiver.deposit(@amount)
+    @status = "complete"
+    counter =+ 1
+    end
+  end
+
+  def reverse_transfer
+
+  end
+
+
 end
