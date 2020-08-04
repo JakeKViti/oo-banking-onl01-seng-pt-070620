1e959016e321c9d821a200585ba1c79d1fd24694
Jacob Viti <thevitij64@gmail.com>
2020-08-04 17:55:36 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -17,17 +17,17 @@ class Transfer
   end
 
   def execute_transaction
-    if valid? == false
-      return "Transaction rejected. Please check your account balance."
-      @status = "rejected"
+    if valid? == true
+      counter = 0
+      until counter != 0
+      @sender.deposit(-@amount)
+      @receiver.deposit(@amount)
+      @status = "complete"
+      counter = 1
+      end
     end
-    counter = 0
-    until counter != 0
-    @sender.deposit(-@amount)
-    @receiver.deposit(@amount)
-    @status = "complete"
-    counter = 1
-    end
+    return "Transaction rejected. Please check your account balance."
+    @status = "rejected"
   end
 
   def reverse_transfer
